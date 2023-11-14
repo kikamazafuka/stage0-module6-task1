7abc3bc9d26b7d2a8b4d0757e1f809cbdc24e8c6
Artur Semenas <uncelfuka@gmail.com>
2023-11-14 23:32:39 +0100

    Module #6. Task #1. Class creation (ref)

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,11 +1,9 @@
 package com.epam.mjc.stage0;
 
 public class Bird extends Animal {
-
     public Bird() {
         super("blue", 2, false);
     }
-
     @Override
     public String getDescription(){
         String str = super.getDescription();
